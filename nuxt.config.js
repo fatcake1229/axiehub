9ba43522a6783af63c312cacd5a620a7581bcba8
fatcake1229 <fatcake1229@protonmail.com>
2021-08-07 21:29:05 +0800

    Added

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,6 +12,7 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Axie Marketplace' },
+      { property: 'og:image', content: "~/assets/Terrarium_leak.png" || '' } ,
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
